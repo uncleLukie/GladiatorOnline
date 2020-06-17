4ab561aff09aaf7f88fc26b7aa02d565f4850f9a
Luke Hewitt <sensator@gmail.com>
2020-06-17 18:36:38 +1000

    Initial commit

---
+++ Source/GladiatorOnline/GladiatorOnline.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "GladiatorOnline.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GladiatorOnline, "GladiatorOnline" );
