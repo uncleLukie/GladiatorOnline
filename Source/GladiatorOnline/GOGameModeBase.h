148c853b52ba42f8532f5cd9ea5efb287354692b
Luke Hewitt <sensator@gmail.com>
2020-06-26 22:57:24 +1000

    fixed dedicated server multiplayer implementation
    GOGameMode now uses OnPostLogin event to obtain the newly joined's PlayerController and Possess a character at random. The randomness is generated by shuffling the get-all-from-class SideScrollerCharacters.

---
+++ Source/GladiatorOnline/GOGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GOGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GLADIATORONLINE_API AGOGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
